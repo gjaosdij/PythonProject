3e7acbb802185c6a5b1716cea6ebb27397e7c5b5
Alex <red3yeshunter@gmail.com>
2021-11-23 22:15:55 -0300

    Update EX059.py

--- EX059.py
+++ EX059.py
@@ -1,19 +1,23 @@
+# opção 4 não funciona corretamente
 cinco = False
 
+print('\033[33m=-='*10, '\033[m')
+v1 = int(input('Primeiro valor: '))
+v2 = int(input('Segundo valor: '))
+
 while not cinco:
-    print('\033[33m=-='*10, '\033[m')
-    v1 = int(input('Primeiro valor: '))
-    v2 = int(input('Segundo valor: '))
-    print('''\033[34m[ 1 ] Somar
+    print('''
+\033[34m[ 1 ] Somar
 [ 2 ] Multiplicar
 [ 3 ] Maior
 [ 4 ] Novos números
 [ 5 ] Sair do programa\033[m''')
     opção = int(input('>>>>> Qual a sua opção? '))
+    print('\033[32m')
     if opção == 1:
-        print(f'{v1} + {v2} = {v1+v2}')
+        print(f'{v1} + {v2} = {v1+v2}\033[m')
     elif opção == 2:
-        print(f'{v1} . {v2} = {v1*v2}')
+        print(f'{v1} . {v2} = {v1*v2}\033[m')
     elif opção == 3:
         if v1 > v2:
             print(f'{v1} é maior que {v2}.')
@@ -22,8 +26,9 @@ while not cinco:
         else:
             print('Nem um dois números é maior, os dois são iguais.')
     elif opção == 4:
-        v1 = int(input('Primeiro valor: '))
+        v1 = int(input('\033[mPrimeiro valor: '))
         v2 = int(input('Segundo valor: '))
     elif opção == 5:
         cinco = True
+    print('\033[33m=-='*10, '\033[m')
 print('Finalizando...')
