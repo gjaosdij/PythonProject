ee67a62be9bd4c6176a3f39b43f0cd00e255286d
Alex <red3yeshunter@gmail.com>
2021-11-24 21:58:56 -0300

    Modifications

--- EX057.py
+++ EX057.py
@@ -1,4 +1,6 @@
 sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()[0]
+
 while sexo not in "FM":
    sexo = input('Dados inválidos. Por favor, informe seu sexo: ').strip().upper()[0]
+
 print(f'Sexo {sexo} registrado com sucesso.')
--- EX059.py
+++ EX059.py
@@ -1,4 +1,4 @@
-# Testar e comparar com a versão do Guanabara
+from time import sleep
 cinco = False
 
 print('\033[33m=-='*10, '\033[m')
@@ -6,14 +6,13 @@ v1 = int(input('Primeiro valor: '))
 v2 = int(input('Segundo valor: '))
 
 while not cinco:
-    print('''
-\033[34m[ 1 ] Somar
+    print('''\033[34m[ 1 ] Somar
 [ 2 ] Multiplicar
 [ 3 ] Maior
 [ 4 ] Novos números
 [ 5 ] Sair do programa\033[m''')
     opção = int(input('>>>>> Qual a sua opção? '))
-    print('\033[32m')
+    print('\033[32m', end='')
     if opção == 1:
         print(f'{v1} + {v2} = {v1+v2}\033[m')
     elif opção == 2:
@@ -26,10 +25,14 @@ while not cinco:
         else:
             print('Nem um dois números é maior, os dois são iguais.')
     elif opção == 4:
+        print('Informe os valores novamente.')
         v1 = int(input('\033[mPrimeiro valor: '))
         v2 = int(input('Segundo valor: '))
     elif opção == 5:
         cinco = True
-    print('Finalizando...')
-    print('\033[33m=-='*10, '\033[m')
+        print('Finalizando...')
+        sleep(2)
+    else:
+        print('Opção inválida. Tente novamente.')
+    print('\033[33m=-='*10, '\033[m')3
 print('Fim do programa! Volte sempre!')
