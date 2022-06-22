d2ae6d10616b9aba8e04822679ebab00be83b491
Alex <red3yeshunter@gmail.com>
2022-06-21 21:20:23 -0300

    Update EX067.py

--- EX067.py
+++ EX067.py
@@ -4,5 +4,7 @@ while n >= 0:
     n = int(input('Quer ver a tabuada de qual valor? '))
     if n < 0:
         break
+    print('-'*30)
     for c in range(0, 11):
         print(f'{n} . {c} = {n*c}')
+    print('-'*30)
