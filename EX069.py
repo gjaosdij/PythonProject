ad0ac3b31304333ea8441b3647af93413768a2f1
Alex <red3yeshunter@gmail.com>
2022-07-25 21:13:16 -0300

    Update EX069.py

--- EX069.py
+++ EX069.py
@@ -1,14 +1,21 @@
-#Concertar ifs e plural e testar se o programa funciona
-
-maisdezoito = homens = mulheres20 = 0
+pessoa = 0
 
 while True:
+    maisdezoito = homens = mulheres20 = 0
+    sexo = ' '
+    idade = -1
+    pessoa += 1
+    
     print('-'*30)
     print('CADASTRE UMA PESSOA')
     print('-'*30)
-
-    idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).strip().upper()
+    print(f'PESSOA {pessoa}\n')
+    
+    while idade < 0:
+        idade = int(input('Idade: '))
+    
+    while sexo not in 'MF':
+        sexo = str(input('Sexo [M/F]: ')).strip().upper()
 
     if idade > 18:
         maisdezoito += 1
@@ -26,5 +33,5 @@ while True:
     elif continuar not in 'SN':
         print('\033[41mERROR\033[m')
 print(f'Total de pessoas com mais de 18 anos: {maisdezoito}')
-print(f'Ao todo temos {homens} cadastrado{'' if homens == 1 else 's'}.')
-print(f'E temos {mulheres20} mulher{'' if mulher == 1 else 'es'} com menos de 20 anos.')
+print('Ao todo temos {} homens cadastrado{}.'.format(homens, '' if homens == 1 else 's'))
+print('E temos {} mulher{} com menos de 20 anos.'.format(mulheres20, '' if mulheres20 == 1 else 's'))
