68997c27b7e115b688cccf8787b9188a319eac0b
Alex <red3yeshunter@gmail.com>
2022-07-25 21:33:43 -0300

    Create EX070.py

---
+++ EX070.py
@@ -0,0 +1,26 @@
+total = maismil = maisbarato = 0
+
+while True:
+    produto = continuar = ' '
+    preço = 0
+
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Preço: '))
+    
+    total += preço
+
+    if preço > 1000:
+        maismil += 1
+    if maisbarato == 0 or maisbarato > preço:
+        maisbarato = preço
+    
+    continuar = str(input('Quer continuar [S/N]?')).strip().upper()
+    
+    if continuar in 'SN':
+        if continuar == 'N':
+            break
+    else:
+        print('ERROR')
+print(f'O total da compra foi {}.')
+print('Temos {} produto{} custando mais que R$1000.00.')
+print('O produto mais barato foi {} que custa R${}.')
