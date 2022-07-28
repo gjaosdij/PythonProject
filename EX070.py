3077a202195e509ff8f4a8d58b5076b786dd5dc5
Alex <red3yeshunter@gmail.com>
2022-07-27 21:25:28 -0300

    Update EX070.py

--- EX070.py
+++ EX070.py
@@ -1,11 +1,15 @@
 total = maismil = maisbarato = 0
 produtobarato = ' '
 
+print('-'*30)
+print('{: ^30}'.format('LOJA SUPER BARATÃO'))
+print('-'*30)
+
 while True:
     produto = continuar = ' '
     preço = 0
 
-    produto = str(input('Nome do produto: '))
+    produto = str(input('Nome do produto: ')).strip()
     preço = float(input('Preço: R$'))
     
     total += preço
@@ -15,14 +19,13 @@ while True:
     if maisbarato == 0 or maisbarato > preço:
         maisbarato = preço
         produtobarato = produto
-    
-    continuar = str(input('Quer continuar [S/N]?')).strip().upper()
-    
-    if continuar in 'SN':
-        if continuar == 'N':
-            break
-    else:
-        print('ERROR')
+
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar [S/N]? ')).strip().upper()
+        if continuar not in 'SN':
+            print('ERROR')
+    if continuar in 'N':
+        break
 print('{:-^40}'.format('FIM DO PROGRAMA'))
 print(f'O total da compra foi {total:.2f}.')
 print('Temos {} produto{} custando mais que R$1000.00.'.format(maismil, '' if maismil == 1 else 's'))
