612461222a597d2974457e0c06cd389cbe9307ee
Alex <red3yeshunter@gmail.com>
2022-08-02 21:19:58 -0300

    Update EX071.py

--- EX071.py
+++ EX071.py
@@ -1,17 +1,31 @@
 #Consertar o programa q ta qbrado pa caraio
 
+from this import d
+
+
 print('='*40)
 print('{: ^40}'.format('BANCO CEV'))
 print('='*40)
 
 valor = float(input('Que valor você quer sacar? R$'))
 
-cinquenta = 0
+total = valor
+céd = 50
+totcéd = 0
 
 while True:
-    while valor % 50 == 0:
-        valordiv = valor/50 
-        cinquenta += 1
-    if valor % 50 != 0:
-        print(f'Notas de R$50.00: {cinquenta}')
-        print(f'Valor restante: R${valordiv}')
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R${céd:.2f}')
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 1
+        totcéd = 0
+        if total == 0:
+            break
