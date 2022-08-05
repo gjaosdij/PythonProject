d337bf7660daf2e965f5127322748d14a93beaf1
Alex <red3yeshunter@gmail.com>
2022-08-04 21:26:48 -0300

    Create EX072.py

---
+++ EX072.py
@@ -0,0 +1,9 @@
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+
+    print(f'Você digitou o número {numeros[n]}.')
+
+    if n == 1963:
+        break
