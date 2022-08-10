07a4a43686d94a8789fc2d61ed2e9da9dba989f9
Alex <red3yeshunter@gmail.com>
2022-08-09 21:33:17 -0300

    Create EX073.py

---
+++ EX073.py
@@ -0,0 +1,5 @@
+times = ('Palmeiras', 'Corinthians', 'Fluminense', 'Athletico-PR', 'Flamengo', 'Internacional', 'Atlético-MG', 'Bragantino', 'Santos', 'América-MG', 'São Paulo', 'Botafogo', 'Goiás', 'Ceará SC', 'Coritiba', 'Avaí', 'Fortaleza', 'Cuiabá', 'Atlético-GO', 'Juventude')
+
+print('-='*58)
+print(f'Lista de times do Brasileirão: {times}')
+print('-='*58)
