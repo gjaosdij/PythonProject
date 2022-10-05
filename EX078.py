15963ee1ff1b6e4802e798ad9d02b2ab34386010
Alex <red3yeshunter@gmail.com>
2022-10-04 22:52:27 -0300

    Changes Whatever

---
+++ EX078.py
@@ -0,0 +1,20 @@
+#Terminar maior e menor
+
+listanum = []
+maior = menor = 0
+
+for c in range(0,5):
+    listanum.append(int(input(f'Digite um número para a posição {c}: ')))
+    if c == 0:
+        maior = menor = listanum[c]
+    else:
+        if maior < listanum[c]:
+            maior = listanum[c]
+        if menor > listanum[c]:
+            menor = listanum[c]
+print('Você digitou os valores', listanum)
+print(f'O maior valor digitado foi {maior} que aparece nas posições', end='')
+for i, v in enumerate(listanum):
+    if v == maior:
+        print(f'{i}... ', end='')
+print(f'O menor valor digitado foi {menor}')
